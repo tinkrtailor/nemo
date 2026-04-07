881c3ef90ca1214e71cbac45d0ba542acb13a68e
tinkrtailor <gunnargylfa@gmail.com>
2026-04-07 16:46:44 +0000

    chore: release 0.2.6 (#52)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -71,19 +71,19 @@ variable "acme_email" {
 variable "control_plane_image" {
   description = "Control plane container image"
   type        = string
-  default     = "ghcr.io/tinkrtailor/nautiloop-control-plane:0.2.5"
+  default     = "ghcr.io/tinkrtailor/nautiloop-control-plane:0.2.6"
 }
 
 variable "agent_base_image" {
   description = "Agent base container image"
   type        = string
-  default     = "ghcr.io/tinkrtailor/nautiloop-agent-base:0.2.5"
+  default     = "ghcr.io/tinkrtailor/nautiloop-agent-base:0.2.6"
 }
 
 variable "sidecar_image" {
   description = "Auth sidecar container image"
   type        = string
-  default     = "ghcr.io/tinkrtailor/nautiloop-sidecar:0.2.5"
+  default     = "ghcr.io/tinkrtailor/nautiloop-sidecar:0.2.6"
 }
 
 variable "k3s_version" {
