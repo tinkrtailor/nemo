9716da68939c01f8e651b432c008b9b75630facc
Gunnar Gylfason <gunnargylfa@gmail.com>
2026-04-08 10:27:16 +0000

    chore: release 0.2.10

--- terraform/examples/existing-server/variables.tf
+++ terraform/examples/existing-server/variables.tf
@@ -50,17 +50,17 @@ variable "acme_email" {
 variable "control_plane_image" {
   description = "Control plane container image"
   type        = string
-  default     = "ghcr.io/tinkrtailor/nautiloop-control-plane:0.2.9"
+  default     = "ghcr.io/tinkrtailor/nautiloop-control-plane:0.2.10"
 }
 
 variable "agent_base_image" {
   description = "Agent base container image"
   type        = string
-  default     = "ghcr.io/tinkrtailor/nautiloop-agent-base:0.2.9"
+  default     = "ghcr.io/tinkrtailor/nautiloop-agent-base:0.2.10"
 }
 
 variable "sidecar_image" {
   description = "Auth sidecar container image"
   type        = string
-  default     = "ghcr.io/tinkrtailor/nautiloop-sidecar:0.2.9"
+  default     = "ghcr.io/tinkrtailor/nautiloop-sidecar:0.2.10"
 }
